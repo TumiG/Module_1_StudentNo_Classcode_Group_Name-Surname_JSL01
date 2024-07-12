0c2284eeb54ded8b62e0813a428d199b51c0615f
TumiG <tumi.ganyane11@gmail.com>
2024-07-12 13:50:15 +0200

    Pet Adoption Syntax

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    const input = document.getElementById('petInput').value.trim();
+    //const result = document.getElementById('result');
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    //const inputValue = input.value.trim();
+    
+    const regex = /^pet_\d{4}[A-Z][a-zA-Z]*$/; 
+    const result = regex.test(input);
+    
+    
+    document.getElementById('result').innerText = result ?"Valid Syntax" : "Invalid Syntax";
 
-            document.getElementById('result').innerText = result;
-}
+         
+    }
 
 
